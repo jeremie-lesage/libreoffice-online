3771668418bc2da0d46b8821e3260025a937a830
Jérémie Lesage <jeremie.lesage@jeci.fr>
2018-11-26 22:41:48 +0100

    add comments

--- README.md
+++ README.md
@@ -18,10 +18,18 @@ docker push jeci/libreoffice-online:$TAG
 If no certificat are present in `/etc/libreoffice-online` a self-signed certificat
 was made by at first start. It is better to create a volume for this directory.
 
+You msut define environment:
+*      LOOL_DOMAIN: my-server\\.domain\\.me
+*      LOOL_USERNAME: lool_admin
+*      LOOL_PASSWORD: lool_password
+
 ``` bash
 docker run --rm -d \
 	-p 9980:9980 \
 	-v "$PWD/etc/:/etc/libreoffice-online" \
+	-e LOOL_DOMAIN=my-server\\.domain\\.me \
+	-e LOOL_USERNAME=lool_admin \
+	-e LOOL_PASSWORD=lool_password \
 	jeci/libreoffice-online:$TAG
 
 ```
--- run-lool.sh
+++ run-lool.sh
@@ -7,26 +7,28 @@
 
 export LC_CTYPE=en_US.UTF-8
 
-# Fix lool resolv.conf problem (wizdude)
-#rm /opt/lool/systemplate/etc/resolv.conf
-#ln -s /etc/resolv.conf /opt/lool/systemplate/etc/resolv.conf
-
 ETC_DIR=/etc/libreoffice-online/
 
-chown lool -R ${ETC_DIR}
 
-# Replace trusted host
-sed -i "s,localhost</host>,${domain}</host>,g" ${ETC_DIR}/loolwsd.xml
-sed -i "s,<username .*></username>,<username>${username}</username>," ${ETC_DIR}/loolwsd.xml
-sed -i "s,<password .*></password>,<password>${password}</password>,g" ${ETC_DIR}/loolwsd.xml
+## Dynamique Configuration
+#    environment:
+#      LOOL_DOMAIN: my-server\\.domain\\.me
+#      LOOL_USERNAME: lool_admin
+#      LOOL_PASSWORD: lool_password
+
+sed -i "s,localhost</host>,${LOOL_DOMAIN}</host>,g" ${ETC_DIR}/loolwsd.xml
+sed -i "s,<username .*></username>,<username>${LOOL_USERNAME}</username>," ${ETC_DIR}/loolwsd.xml
+sed -i "s,<password .*></password>,<password>${LOOL_PASSWORD}</password>,g" ${ETC_DIR}/loolwsd.xml
 
 sed -i "s,true</seccomp>,false</seccomp>,g" ${ETC_DIR}/loolwsd.xml
 sed -i "s,localhost:9042/foo</monitor>,loolmonitor:8765</monitor>,g" ${ETC_DIR}/loolwsd.xml
 
 sed -i 's,/etc/loolwsd,/etc/libreoffice-online,' ${ETC_DIR}/loolwsd.xml
+
+## Generate Cert
 /generateSSL.sh
 
-# Start loolwsd
+## Start loolwsd
 LOOL_PARAM="--version"
 LOOL_PARAM="${LOOL_PARAM} "
 LOOL_PARAM="${LOOL_PARAM} --o:sys_template_path=/opt/lool/systemplate "
